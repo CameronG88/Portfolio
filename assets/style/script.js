0116f73d0a5e71408de5eb5ee5ca99b2e2d99564
Cameron Graham <cameron.g882@gmail.com>
2020-07-01 23:57:20 -0400

    linked all pages gave every link cursor:pointer changed picture for about me page

--- assets/style/script.js
+++ assets/style/script.js
@@ -7,3 +7,22 @@ $("#weatherhubCaption").click(function() {
     var URL = "https://camerong88.github.io/WeatherHub/"
     window.open(URL,'_blank','',''); 
 });
+
+$("#githubLink").click(function() {
+    var URL = "https://github.com/CameronG88"
+    window.open(URL,'_blank','',''); 
+});
+
+$("#linkedinLink").click(function() {
+    var URL = "https://www.linkedin.com/in/cameron-graham-a658851a8/"
+    window.open(URL,'_blank','',''); 
+});
+
+$("#resumeLink").click(function() {
+    var URL = "./assets/Resume.pdf"
+    window.open(URL,'_blank','',''); 
+});
+$("#camTitle").click(function() {
+    var URL = "./index.html"
+    window.open(URL,'','',''); 
+});
