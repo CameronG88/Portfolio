0d7cfbbac968de2c92c9d72aabab0ea2792619a1
Cameron Graham <cameron.g882@gmail.com>
2020-07-01 23:21:54 -0400

    fixed up portfolio description and linked captions to the project links

---
+++ assets/style/script.js
@@ -0,0 +1,9 @@
+$("#bandifyCaption").click(function() {
+    var URL = "https://camerong88.github.io/Bandify/"
+    window.open(URL,'_blank','',''); 
+});
+
+$("#weatherhubCaption").click(function() {
+    var URL = "https://camerong88.github.io/WeatherHub/"
+    window.open(URL,'_blank','',''); 
+});
